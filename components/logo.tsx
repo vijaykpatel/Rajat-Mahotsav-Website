6a59c66ca731362c926d7411780352c00a7dbe17
Vijay Patel <vijpatel7@gmail.com>
2025-09-08 14:40:54 -0400

    Mobile resizing

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,15 @@
 "use client"
 
 import Image from "next/image"
+import { useDeviceType } from "@/hooks/use-device-type"
 
 export default function Logo() {
+  const deviceType = useDeviceType()
+  
+  const logoClasses = deviceType === 'mobile' 
+    ? "max-w-[80vw] max-h-full w-auto h-auto object-contain landscape:max-w-[50vw] landscape:max-h-[40vh]"
+    : "max-w-[80vw] max-h-full w-auto h-auto object-contain"
+
   return (
     <div className="text-center w-full h-full flex items-center justify-center">
       <Image
@@ -10,7 +17,7 @@ export default function Logo() {
         alt="Rajat Mahotsav Logo"
         width={350}
         height={225}
-        className="max-w-[80vw] max-h-full w-auto h-auto object-contain"
+        className={logoClasses}
         priority
       />
     </div>
