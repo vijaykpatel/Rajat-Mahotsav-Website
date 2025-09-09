c045ced054142d39a21c43cf8ddd40609dcccfa7
Vijay Patel <vijpatel7@gmail.com>
2025-09-09 11:40:07 -0400

    Fixe bar from reappearing when scroll stops on mobile

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -60,10 +60,8 @@ export function Navigation() {
       
       if (currentScrollY < 10) {
         setIsVisible(true)
-      } else if (currentScrollY > lastScrollY && currentScrollY > 100) {
+      } else {
         setIsVisible(false)
-      } else if (currentScrollY < lastScrollY) {
-        setIsVisible(true)
       }
       
       setLastScrollY(currentScrollY)
