065740d8b6947c8b52d7a372708066c1a5aa7670
Vijay Patel <vijpatel7@gmail.com>
2026-02-14 12:15:39 -0500

    Investigate build warnings

--- components/organisms/invitation-pdf-viewer.tsx
+++ components/organisms/invitation-pdf-viewer.tsx
@@ -215,7 +215,7 @@ export function InvitationPdfViewer({ filePath, title, languageKey }: Invitation
         renderTaskRef.current = null
       }
       if (previousDocument) {
-  }, [computeFitScale, filePath])
+        void previousDocument.destroy().catch(() => undefined)
         previousDocument = null
       }
     }
